19de80beeeacca6e0017563ec4a5c6b293a33979
dhiru0904 <dhirajdevre4@gmail.com>
2022-11-25 14:42:57 +0530

    Added: program print even or odd number

---
+++ src/basiccorejavaprogram/EvenOdd.java
@@ -0,0 +1,15 @@
+package basiccorejavaprogram;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int number = scanner.nextInt();
+        if (number % 2 == 0)
+            System.out.println(number + " is even");
+        else
+            System.out.println(number + " is odd");
+    }
+}
